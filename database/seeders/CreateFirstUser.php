ccbde4c741c9a989ff8263a49908d7fcbc9a3887
febby24si-create <febby24si@mahasiswa.pcr.ac.id>
2025-11-11 02:12:20 +0700

    membuat seeding untuk user

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Fahrezzy',
+            'email' => 'Reezyfbi@gmail.com',
+            'password' => 'password123', // tidak perlu Hash::make
+            'role' => 'admin',           // tambahkan kalau kolom role ada
+        ]);
+    }
+}
